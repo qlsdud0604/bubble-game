a90dac2a8278fff2f61ed5adbf313a6b43b00ba5
임빈영 <qlsdud960604@gmail.com>
2022-01-21 12:28:29 +0900

    캐릭터 바닥 충돌 시 정지시키기

---
+++ src/test/ex08/BackgroundPlayerService.java
@@ -0,0 +1,64 @@
+package test.ex08;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+public class BackgroundPlayerService implements Runnable {
+
+	private BufferedImage image;
+	private Player player;
+
+	public BackgroundPlayerService(Player player) {
+		this.player = player;
+
+		try {
+			image = ImageIO.read(new File("images/backgroundMapService.png"));
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Override
+	public void run() {
+		while (true) {
+			Color leftColor = new Color(image.getRGB(player.getX() - 10, player.getY() + 25));
+			Color rightColor = new Color(image.getRGB(player.getX() + 65, player.getY() + 25));
+			int bottomColor = image.getRGB(player.getX() + 10, player.getY() + 55)
+					+ image.getRGB(player.getX() + 40, player.getY() + 55);
+
+			/** 바닥 충돌 감지 */
+			if (bottomColor != -2) {
+				// System.out.println("바닥 충돌");
+				player.setDown(false);
+			}
+
+			/** 외벽 충돌 감지 */
+			if (leftColor.getRed() == 255 && leftColor.getGreen() == 0 && leftColor.getBlue() == 0) {
+				// System.out.println("왼쪽 벽 충돌");
+				player.setLeftWallCrash(true);
+				player.setLeft(false);
+			} else if (rightColor.getRed() == 255 && rightColor.getGreen() == 0 && rightColor.getBlue() == 0) {
+				// System.out.println("오른쪽 벽 충돌");
+				player.setRightWallCrash(true);
+				player.setRight(false);
+			} else {
+				player.setLeftWallCrash(false);
+				player.setRightWallCrash(false);
+			}
+
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+}
+
+// 1. 메인 스레드는 키보드 이벤트를 처리해야 하기 때문에 바쁨
+// 2. 백그라운드에서 
